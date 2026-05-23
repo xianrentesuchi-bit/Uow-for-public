c22a89fccc8f6b57f001fd2a370440e27d19d1cf
xianrentesuchi-bit <xianrentesuchi@gmail.com>
2026-05-23 19:33:06 +0900

    Update VideoPlayer.vue

--- src/components/video/VideoPlayer.vue
+++ src/components/video/VideoPlayer.vue
@@ -1,13 +1,19 @@
+<!-- components/video/VideoPlayer.vue -->
 <script setup lang="ts">
 defineProps<{
   id: string
+  stream?: string
 }>()
 </script>
 
 <template>
   <iframe
-    class="w-full aspect-video rounded-2xl"
-    :src="`https://www.youtube.com/embed/${id}`"
+    class="w-full aspect-video rounded-2xl border border-zinc-200"
+    :src="
+      stream === 'youtube'
+        ? `https://www.youtube.com/embed/${id}`
+        : `https://www.youtube-nocookie.com/embed/${id}`
+    "
     allowfullscreen
   />
 </template>
