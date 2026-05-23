505523adbbc3fbf047bb322a3eeab8cb40745cfb
xianrendesu-max <xianrendesu@gmail.com>
2026-05-23 17:53:32 +0900

    Update Loading.vue

--- src/components/layout/Loading.vue
+++ src/components/layout/Loading.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="fixed inset-0 z-[9999] bg-[#0f0f0f] flex items-center justify-center">
+  <div class="fixed inset-0 z-[9999] bg-[#f9f9f9] flex items-center justify-center">
     <div class="flex flex-col items-center">
 
       <div
-        class="w-16 h-16 border-4 border-zinc-700 border-t-red-500 rounded-full animate-spin"
+        class="w-16 h-16 border-4 border-zinc-200 border-t-red-500 rounded-full animate-spin"
       ></div>
 
-      <div class="mt-6 text-xl font-semibold text-white">
+      <div class="mt-6 text-xl font-semibold text-zinc-900">
         動画読み込み中...
       </div>
 
-      <div class="mt-2 text-zinc-400">
+      <div class="mt-2 text-zinc-500">
         動画検索やデータ取得をしています
       </div>
 
