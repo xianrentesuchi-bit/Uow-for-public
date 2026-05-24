696a5cef05f012b21f39e9077d725660f9b74470
xianrendesu-max <xianrendesu@gmail.com>
2026-05-24 10:31:19 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,16 @@
 <template>
+  <Header @toggle-sidebar="isSidebarOpen = $event" />
+
+  <Sidebar :isOpen="isSidebarOpen" />
+
   <router-view />
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+import Header from './components/Header.vue'
+import Sidebar from './components/Sidebar.vue'
+
+const isSidebarOpen = ref(false)
+</script>
